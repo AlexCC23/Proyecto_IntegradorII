902958d74493a29fe12dada2ec5d63f46546f16e
Alex Chinchay <alexchinchay41@gmail.com>
2024-04-11 12:55:40 -0500

    configuraciones

---
+++ hospital/ReceteMedica/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecetemedicaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ReceteMedica'
--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+LOCAL_APPS = [
+    'ReceteMedica',
+]
+
+THIRD_APPS = [
+    'rest_framework',
+    "corsheaders",
+]
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,6 +76,12 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]           
 
 WSGI_APPLICATION = 'hospital.wsgi.application'
 
@@ -121,3 +137,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny' 
+    ]
+}
