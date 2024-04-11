4809f6f1a5d165c7abd518568a684dfa6f55766c
Alex Chinchay <alexchinchay41@gmail.com>
2024-04-11 16:23:47 -0500

    API_RECETA

--- hospital/ReceteMedica/views.py
+++ hospital/ReceteMedica/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Receta #modelo para crear la BD
+from .serializers import RecetaSerializers #modelo del API, los campos que ve a traer la API de la BD 
 
 # Create your views here.
+
+#trae todos los datos del modelo Receta,
+class RecetaView(viewsets.ModelViewSet):
+    serializer_class=RecetaSerializers
+    queryset=Receta.objects.all() #este sentencia es oara trear todos los datos de los campos indicados en serializers
+     #este es como el json
--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Hospital",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
