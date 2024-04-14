4b781bf93c9aee2e6538de7f311d8e97accf781e
jxdflores25 <jxdflores25@gmail.com>
2024-04-13 19:28:53 -0500

    Mejorando Modelo Hospital

--- hospital/ReceteMedica/models.py
+++ hospital/ReceteMedica/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Receta(models.Model):
     dni_Paciente=models.CharField(max_length=10)
     nombre_Medico=models.CharField(max_length=150)
+    fecha=models.DateField(auto_now_add=True)
     
