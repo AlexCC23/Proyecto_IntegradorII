1330e77dfff10b383ecdaeb44bbf513ebe4bda64
Alex Chinchay <alexchinchay41@gmail.com>
2024-04-17 11:01:20 -0500

    Union de front con back

--- Seguro/Seguro/settings.py
+++ Seguro/Seguro/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    
+    "http://localhost:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]   
@@ -97,7 +97,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "Seguro",
         "USER": "postgres",
-        "PASSWORD": "Gordy25@",
+        "PASSWORD": "admin",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "Hospital",
         "USER": "postgres",
-        "PASSWORD": "Gordy25@",
+        "PASSWORD": "admin",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
